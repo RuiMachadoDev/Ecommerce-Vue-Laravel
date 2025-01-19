6453fb2044cf544a568c8a447bfbf6e0818b443c
RuiMachadoDev <ruifilipemachado@hotmail.com>
2025-01-19 15:50:40 +0000

    Atualização para resolução de bugs na criação de categorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -25,7 +25,7 @@ class CategoryController extends Controller
         try {
             $validated = $request->validate([
                 'name' => 'required|string|max:255',
-                'slug' => 'required|string|unique:categories,slug',
+                'slug' => 'required|string',
             ]);
     
             $category = Category::create($validated);
